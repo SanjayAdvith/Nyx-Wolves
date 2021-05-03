18e54a5d8be26d407530b34595cec94ec5def9ff
sanjay <skengg9824@gmail.com>
2021-05-03 17:37:47 +0530

    setup of MongoDB atlas and connection with databse

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
 
 const app = express()
+dotenv.config()
+connectDB()
 
 app.get('/', (req, res) => {
     res.send('api is running...')
